5e15419f548ac273344cf8e8e6052f7c87ffa773
xaxipiruli <frapercan1@alum.us.es>
2022-12-14 15:08:34 +0100

    limpieza control log

--- personajes/personajes.service.js
+++ personajes/personajes.service.js
@@ -10,7 +10,6 @@ async function getAll(req) {
   const limit = parseInt(req.query.limit, 10) || 10;
   const page = parseInt(req.query.page, 10) || 10;
   const filter = req.query.filter != "undefined" ? req.query.filter : "";
-  console.log(req.query);
   const paginacionPersonajes = await Personajes.paginate(
     { name: { $regex: filter, $options: "i" } },
     { limit, page }
