db189fe69deec9cbcadac7e183942c4f7947cc41
idavid80 <dominguezbueno.david@gmail.com>
2023-01-01 18:39:33 +0100

    mejor valorado terminado, pendiente favorito

--- valoraciones/valoraciones.service.js
+++ valoraciones/valoraciones.service.js
@@ -4,7 +4,6 @@ const Valoracion = db.Valoraciones;
 const Personaje = db.Personajes;
 const ObjectId = require("mongodb").ObjectId;
 
-
 module.exports = {
   get,
   create,
@@ -13,7 +12,7 @@ module.exports = {
 };
 
 async function get(req) {
-  console.log(req.query)
+  console.log(req.query);
   let valoracion = await Valoracion.findOne({
     personaje: ObjectId(req.query.idPersonaje),
     idUsuario: req.query.idUsuario,
@@ -25,14 +24,14 @@ async function get(req) {
   }
 }
 
- async function create(req) {
+async function create(req) {
   let personaje = await Personaje.findOne({ name: req.query.personaje });
 
   let valoracion = await Valoracion.findOne({
     idUsuario: req.body.idUsuario,
     personaje: req.body.idPersonaje,
   });
-  
+
   if (!valoracion) {
     valoracion = Valoracion.create({
       idUsuario: req.body.idUsuario,
@@ -43,15 +42,36 @@ async function get(req) {
     valoracion.valoracion = req.body.valoracion;
     valoracion.save();
   }
-  console.log(valoracion)
+  console.log(valoracion);
   return valoracion;
-} 
+}
 
 async function mejorValorado() {
-  const valoraciones = await Valoracion.find().populate('valoracion').sort({valoracion: -1}).limit(5);
-  return valoraciones
+  const valoraciones = await Valoracion.find()
+    .populate("valoracion")
+    .sort({ valoracion: -1 })
+    .limit(5);
+
+  let respuesta = [];
+  for (let i = 0; i < valoraciones.length; i++) {
+    let id = Object(valoraciones[i].personaje);
+    let personaje = await Personaje.findById({ _id: id });
+    let datos = {
+      valoracion: valoraciones[i].valoracion,
+      personaje: personaje.name,
+      imagen: personaje.thumbnail.path + "." + personaje.thumbnail.extension,
+    };
+    respuesta.push(datos);
+  }
+console.log(respuesta)
+  return respuesta;
 }
+
 async function favoritos(req) {
-  const miPersonaje = await Valoracion.find({idUsuario: req.body.idUsuario}).populate('valoracion').sort({valoracion: -1}).limit(5);
-  return miPersonaje
+  const miPersonaje = await Valoracion.find({ idUsuario: req.body.idUsuario })
+    .populate("valoracion")
+    .sort({ valoracion: -1 })
+    .limit(5);
+  console.log("miPersonaje", miPersonaje);
+  return miPersonaje;
 }
