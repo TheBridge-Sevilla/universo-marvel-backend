12a8afa9b437628560ffddb258fa91734856f49e
Ulyssbat <stewartbatallasminda@gmail.com>
2023-01-04 19:12:50 +0100

    comentarios service mejorado

--- comentarios/comentarios.service.js
+++ comentarios/comentarios.service.js
@@ -7,41 +7,25 @@ const ObjectId = require("mongodb").ObjectId;
 module.exports = {
     get,
     create,
-    favoritos,
-    mejorValorado,
 };
 
 async function get(req) {
     console.log(req.query);
-    let comentario = await Comentario.findOne({
-        personaje: ObjectId(req.query.idPersonaje),
-        idUsuario: req.query.idUsuario,
+    let comentarios = await Comentario.find({
+        personaje: ObjectId(req.query.idPersonaje)
     });
-    if (comentario) {
-        return comentario.comentario;
-    } else {
-        return 0;
-    }
+    return comentarios
 }
 
 async function create(req) {
-    let personaje = await Personaje.findOne({ name: req.query.personaje });
-
-    let comentario = await Comentario.findOne({
+    console.log(req.query)
+    let personaje = Personaje.findOne({
+        personaje: req.body.idPersonaje
+    })
+    let comentario = Comentario.create({
         idUsuario: req.body.idUsuario,
-        personaje: req.body.idPersonaje,
-    });
-
-    if (!comentario) {
-        comentario = Comentario.create({
-            idUsuario: req.body.idUsuario,
-            comentario: req.body.comentario,
-            personaje: personaje._id,
-        });
-    } else {
-        comentario.comentario = req.body.comentario;
-        comentario.save();
-    }
-    console.log(comentario);
-    return comentario;
+        comentario: req.body.comentario,
+        personaje: personaje._id,
+    })
+    return comentario
 }
